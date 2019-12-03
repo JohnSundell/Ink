a60a7b85e94ba88cfa949485192a587dc445d779
steve-h <stevelist@hume.ca>
2019-12-03 11:04:39 -0500

    change <hr/> to HTML5 <hr>

--- Tests/InkTests/HorizontalLineTests.swift
+++ Tests/InkTests/HorizontalLineTests.swift
@@ -17,12 +17,12 @@ final class HorizontalLineTests: XCTestCase {
         World
         """)
 
-        XCTAssertEqual(html, "<p>Hello</p><hr/><p>World</p>")
+        XCTAssertEqual(html, "<p>Hello</p><hr><p>World</p>")
     }
 
     func testHorizontalLineWithDashesAtTheStartOfString() {
         let html = MarkdownParser().html(from: "---\nHello")
-        XCTAssertEqual(html, "<hr/><p>Hello</p>")
+        XCTAssertEqual(html, "<hr><p>Hello</p>")
     }
 
     func testHorizontalLineWithAsterisks() {
@@ -34,7 +34,7 @@ final class HorizontalLineTests: XCTestCase {
         World
         """)
 
-        XCTAssertEqual(html, "<p>Hello</p><hr/><p>World</p>")
+        XCTAssertEqual(html, "<p>Hello</p><hr><p>World</p>")
     }
 }
 
